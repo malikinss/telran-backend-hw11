12414d364168f959d9d2f0a4a576dd05cde2bf36
Sam Malikin <workmalikin@gmail.com>
2025-11-02 19:29:17 +0200

    Delete index.ts

--- src/__tests__/employeeRoutesTests/index.ts
+++
@@ -1,6 +0,0 @@
-// src/__tests__/employeeRoutesTests/index.ts
-
-export { testGetEmployees } from "./employeeGetRoutes.test.ts";
-export { testPostEmployee } from "./employeePostRoutes.test.ts";
-export { testPatchEmployee } from "./employeePatchRoutes.test.ts";
-export { testDeleteEmployee } from "./employeeDeleteRoutes.test.ts";
